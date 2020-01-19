7eb5759d1e8effe6a792f9cb4a85ffbe847b2b05
Final <final@yiji.com>
2020-01-19 09:03:33 +0800

    1.basic
    2.method(sample_method)

--- input.py
+++
@@ -1,3 +0,0 @@
-print('请输入你的名字')
-name = input()
-print('你的名字是' ,name)
---
+++ method/math_method.py
@@ -0,0 +1,20 @@
+print(abs(-1))
+
+print(max(2, 3, 1, -5))
+
+
+###数据类型转换
+print(int('123'))
+print(int(12.7))
+
+
+print(str(1.23))
+
+
+print(bool(1))
+
+print(bool(0))
+
+###函数起了一个别名
+a = abs # 变量a指向abs函数
+print(a(-1))
